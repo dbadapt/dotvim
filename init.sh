58aaab1c3f2e9611b06fcff0826f3c859110bc40
David Bennett <david.bennett@percona.com>
2015-08-09 18:10:23 +0000

    Init script will now compile YCM

--- init.sh
+++ init.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+VIMVER=$(vim --version | head -n1 | sed -r 's/^.*([0-9]+\.[0-9]+).*$/\1/')
+
 cd ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git Vundle.vim
 vim +PluginInstall +qall
+
+# build YCM if we are oin 7.3 or greater
+if awk "BEGIN{exit ${VIMVER} >= 7.3 ? 0 : 1}"; then
+  # for ycm
+  if hash apt-get; then
+    apt-get update -y
+    apt-get install -y clang cmake xz-utils python-all-dev
+  fi
+
+  cd ~/.vim/bundle/YouCompleteMe
+  ./install.sh --clang-completer
+fi
+exit;
